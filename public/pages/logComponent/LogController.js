33efb3241cc75785e22414be743a005fbf71d894
NeseAtes <nese.ates97@gmail.com>
2019-03-15 17:46:34 +0200

    son

--- public/pages/logComponent/LogController.js
+++ public/pages/logComponent/LogController.js
@@ -1,4 +1,4 @@
-app.controller('LogController', function($scope, $http, SERVICE_URL, DTOptionsBuilder, DTColumnDefBuilder, $location) {
+app.controller('LogController', function($scope, $http, $interval, SERVICE_URL, DTOptionsBuilder, DTColumnDefBuilder, $location) {
     $scope.logs = [];
     $scope.users =[];
 
@@ -38,12 +38,11 @@ app.controller('LogController', function($scope, $http, SERVICE_URL, DTOptionsBu
     }
 
     $scope.getAllLogs()
-
     
 
 
     $scope.yolla = function(){
-        
+
         var data = {
             value : $scope.elastic
         };
@@ -52,6 +51,7 @@ app.controller('LogController', function($scope, $http, SERVICE_URL, DTOptionsBu
         .then(function(resp){
             console.log("ES sonuc: ", resp.data.hits);
         $scope.logs=[];
+        $scope.users=[];
             console.log($scope.logs); 
             if ($scope.elastic == "") {
                 $scope.getAllLogs()
@@ -61,16 +61,36 @@ app.controller('LogController', function($scope, $http, SERVICE_URL, DTOptionsBu
                var a = resp.data.hits[i]._source;
                console.log(resp.data.hits[i]._source);
                $scope.logs.push(a);
+               $scope.users.push(a);
             }
             }           
             $scope.logs=[];
-            
+            $scope.users=[];
+
             for (var i = 0; i < resp.data.hits.length; i++) {
                var a = resp.data.hits[i]._source;
                $scope.logs.push(a);
+               $scope.users.push(a);
             } 
         },function(err){
             console.log("ES err: ", err);
         });
     }
+
+    
+    /*var i = 1;
+    $scope.ellipsisText = "But wait, there's more";
+
+    $scope.paragraphText = "Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal. Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live. It is altogether fitting and proper that we should do this. But, in a larger sense, we can not dedicate, we can not consecrate, we can not hallow, this ground. The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced.";
+
+    $interval(function() {
+        $scope.incrementing = "But wait, there's more x" + i;
+        i++;
+    }, 1000);
+
+    $scope.showFullText = function() {
+        alert('On click function');
+    };*/
+
+
 });
